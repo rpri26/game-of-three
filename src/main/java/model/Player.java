7284acb991039d159cb030b9b2320b9de903b862
rpri26 <56364510+rpri26@users.noreply.github.com>
2020-12-16 14:31:18 +0100

    Create Player.java

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Player {
+	
+	String name;
+	PlayMode mode;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public PlayMode getMode() {
+		return mode;
+	}
+	public void setMode(PlayMode mode) {
+		this.mode = mode;
+	}
+
+}
