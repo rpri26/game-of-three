b3d4f3201598a828747243c37fbcda081325895d
rpri26 <56364510+rpri26@users.noreply.github.com>
2020-12-16 14:35:01 +0100

    Create Constants.java

---
+++ src/main/java/util/Constants.java
@@ -0,0 +1,9 @@
+package util;
+
+public class Constants {
+	public static final int WINNER_NUMBER = 1;
+	public static final String DONE_KEYWORD = "DONE";
+	public static final int SUCCESS_STATUS = 0;
+	public static final int FAILURE_STATUS = 1;
+
+}
